9f87ecb4717a413837a9faf13ee010ffccb9d8b4
Jhonny <jhonnycgarcia@gmail.com>
2025-05-03 22:06:03 -0500

    rutas y stack navigator

---
+++ src/MapsApp.tsx
@@ -0,0 +1,13 @@
+import './gesture-handler.native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { StackNavigator } from './presentation/navigation/StackNavigator';
+
+
+export const MapsApp = () => {
+    return (
+        <NavigationContainer>
+            <StackNavigator />
+        </NavigationContainer>
+    );
+};
