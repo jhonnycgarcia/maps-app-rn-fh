824a3805331f1bd2c8c1c035eedcf08be90e3478
Jhonny <jhonnycgarcia@gmail.com>
2025-05-03 22:38:50 -0500

    revisar permiso de ubicacion

--- src/actions/permissions/location.ts
+++ src/actions/permissions/location.ts
@@ -1,6 +1,14 @@
-import { openSettings, PERMISSIONS, request, PermissionStatus as RNPermissionStatus } from 'react-native-permissions';
+import { check, openSettings, PERMISSIONS, request, PermissionStatus as RNPermissionStatus } from 'react-native-permissions';
 import type { PermissionStatus } from '../../infrastructure/interfaces/permission';
 import { Platform } from 'react-native';
+
+const permissionMapper: Record<RNPermissionStatus, PermissionStatus> = {
+    granted: 'granted',
+    denied: 'denied',
+    blocked: 'blocked',
+    limited: 'limited',
+    unavailable: 'unavailable',
+};
 
 export const requestLocationPermission = async (): Promise<PermissionStatus> => {
     let status: RNPermissionStatus = 'unavailable';
@@ -20,13 +28,19 @@ export const requestLocationPermission = async (): Promise<PermissionStatus> => 
         //TODO: return await checkLocationPermission();
     }
 
-    const permissionMapper: Record<RNPermissionStatus, PermissionStatus> = {
-        granted: 'granted',
-        denied: 'denied',
-        blocked: 'blocked',
-        limited: 'limited',
-        unavailable: 'unavailable',
-    };
+    return permissionMapper[status] ?? 'unavailable';
+};
+
+export const checkLocationPermission = async (): Promise<PermissionStatus> => {
+    let status: RNPermissionStatus = 'unavailable';
+
+    if (Platform.OS === 'ios') {
+        status = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    } else if (Platform.OS === 'android') {
+        status = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+    } else {
+        throw new Error('Unsupported platform');
+    }
 
     return permissionMapper[status] ?? 'unavailable';
 };
