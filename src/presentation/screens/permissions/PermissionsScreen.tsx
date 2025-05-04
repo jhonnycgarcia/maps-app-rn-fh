9c6b874d80c6763daf1d45f45ddbafb525b139ff
Jhonny <jhonnycgarcia@gmail.com>
2025-05-03 23:40:45 -0500

    Consumir nuestro store

--- src/presentation/screens/permissions/PermissionsScreen.tsx
+++ src/presentation/screens/permissions/PermissionsScreen.tsx
@@ -1,9 +1,21 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
+import { globalStyles } from '../../../config/theme/styles';
+import { userPermissionStore } from '../../store/permissions/usePermissionStore';
 
 export const PermissionsScreen = () => {
+    const { locationStatus, requestLocationPermission } = userPermissionStore();
+
     return (
-        <View>
-            <Text>PermissionsScreen</Text>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Habilitar ubicación</Text>
+            <Pressable
+                style={globalStyles.btnPrimary}
+                onPress={requestLocationPermission}
+            >
+                <Text style={{ color: 'white'}}>Habilitar Localización</Text>
+            </Pressable>
+
+            <Text>Estado actual: {locationStatus}</Text>
         </View>
     );
 };
